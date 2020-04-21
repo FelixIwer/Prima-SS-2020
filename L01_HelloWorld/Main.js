fd064a7edabd661febd5a4fc136079df95102af0
FelixIwer <49561587+FelixIwer@users.noreply.github.com>
2020-04-21 18:09:45 +0200

    First Lessen End

--- L01_HelloWorld/Main.js
+++ L01_HelloWorld/Main.js
@@ -1,2 +1,8 @@
 "use strict";
+var L01_HelloWorld;
+(function (L01_HelloWorld) {
+    var fudge = FudgeCore;
+    let node = new fudge.Node("TestNode");
+    console.log(node);
+})(L01_HelloWorld || (L01_HelloWorld = {}));
 //# sourceMappingURL=Main.js.map
--- L01_HelloWorld/Main.ts
+++ L01_HelloWorld/Main.ts
@@ -1,4 +1,7 @@
-namespace HelloWorld
+namespace L01_HelloWorld
 {
+    import fudge = FudgeCore;
 
+    let node: fudge.Node = new fudge.Node("TestNode");
+    console.log(node);
 }
